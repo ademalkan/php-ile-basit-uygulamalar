07427d0e41cc4b631365958bdd31e8d2a47e1460
ademalkan <43451577+ademalkan@users.noreply.github.com>
2020-05-22 22:00:37 +0300

    Create ehliyet.php
    If sorgusu ile kullanıcının ehliyet alıp alamayacağını ekrana basan uygulama.

---
+++ ehliyet.php
@@ -0,0 +1,12 @@
+<?php
+$yas = 1;
+
+if ($yas >= 18 && $yas >= 18) {
+    echo "Ehliyet alabilirsiniz.";
+} else if ($yas < 18) {
+    echo "Ehliyet alamazsınız.";
+} else {
+    echo "Hatalı giriş.";
+}
+    
+?>
