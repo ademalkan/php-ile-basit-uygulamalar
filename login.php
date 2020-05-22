073a0b4faa31603d7d6c8a0d95ed067c5096a8ce
ademalkan <43451577+ademalkan@users.noreply.github.com>
2020-05-22 22:01:15 +0300

    Create login.php
    Dizi içerisinde panel bilgilerini alıyoruz ve if sorgusu ile girilen bilgilerin doğruluğunu teyit eden uygulama...

---
+++ login.php
@@ -0,0 +1,15 @@
+<?php
+
+$dizi = array("admin", "password");
+
+$kadi = "admin";
+$sifre = "password";
+
+if ($dizi[0] == $kadi and $dizi[1] == $sifre) {
+    echo "Giriş Başarılı";
+
+} else {
+    echo "Girdiğiniz bilgileri kontrol ediniz.";
+}
+ 
+?>
